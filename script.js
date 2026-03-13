9b2a9a9761dc4a0defaacd64053bbf2c0f9ac506
Vinícius Blesa dos Santos <blesa1269@gmail.com>
2026-03-13 19:33:02 -0300

    atualizando cadastro com uma nova função remover usuario

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-//alert("Tudo pronto para programar Sr. Vinicius");
+//alert("Tudo pronto para programar Sr. Vinicius!");
 
 let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-function adicionarUsuario(){
+function cadastrarUsuario(){
     let nome = document.getElementById("nome").value;
     let idade = document.getElementById("idade").value;
 
+    let novoUsuario = {nome: nome, idade: idade};
+
     if (nome === "" || idade === ""){
-        alert("Preencha Todos os Campos");
+        alert("Preencha todas as informações");
         return;
     }
-
-    let novoUsuario = {nome: nome, idade: idade};
 
     usuarios.push(novoUsuario);
 
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-    mostrarUsuarios();
+    mostrarUsuarios()
 }
 
 function mostrarUsuarios(){
-    let lista = document.getElementById("listaUsuarios");
-    
+    let lista = document.getElementById("lista");
+
     lista.innerHTML = "";
 
-    usuarios.forEach(usuario => {
+    usuarios.forEach((usuario, index) => {
         let item = document.createElement("li");
+        item.textContent = usuario.nome + " - " + usuario.idade + " anos ";
 
-        item.textContent = usuario.nome + " - " + usuario.idade + " anos";
+        let botao = document.createElement("button");
+        botao.textContent = "Remover";
 
+        botao.onclick = function(){
+            removerUsuario(index);
+        }
+
+        item.appendChild(botao);
         lista.appendChild(item);
-    })
+    });
+}
+
+function removerUsuario(index){
+    usuarios.splice(index, 1);
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    mostrarUsuarios();
 }
 
 mostrarUsuarios();
